1c6c228b4631f5b7e3de371cff5d942e624dfdc3
Tanguy <tanguy.blochet@viacesi.fr>
2018-10-10 10:27:13 +0200

    first commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	void OnEnable()
+	{
+		if (PupilTools.IsConnected)
+		{
+			// Show the three colored dots to visualize the gaze
+			PupilGazeTracker.Instance.StartVisualizingGaze ();		
+			// Subscribe to gaze
+			PupilTools.SubscribeTo("Gaze");
+			// print the gaze position in the world
+			print(PupilData._2D.GazePosition.ToString("F2"));
+			
+		}
+	}
+}
---
+++ Assets/Test_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnEnable()
+	{
+		if (PupilTools.IsConnected)
+		{
+			// Show the three colored dots to visualize the gaze
+			PupilGazeTracker.Instance.StartVisualizingGaze ();		
+			// Subscribe to gaze
+			PupilTools.SubscribeTo("Gaze");
+			// print the gaze position in the world
+			print(PupilData._2D.GazePosition.ToString("F2"));
+			
+		}
+	}
+}
